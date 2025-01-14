1e6ed7af61a6dbf2ee53553131949fe203ef6867
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2025-01-14 17:26:19 +0100

    upgrade: project dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,13 @@
-import foxkit from "eslint-config-foxkit/configs/base.js";
+import foxkit from "eslint-config-foxkit/flat.js";
 import prettier from "eslint-config-prettier";
-
-const __dirname = new URL(".", import.meta.url).pathname.slice(0, -1);
 
 /**
  * @see https://github.com/foxkit-js/eslint-config-foxkit/ for more information
  */
 export default [
-  { ignores: ["dist/**"] },
+  { ignores: ["dist/**", "logs/**"] },
   foxkit.base,
   foxkit.typescript,
-  foxkit.configureTS({ tsconfigRootDir: __dirname }),
+  foxkit.configureTS({ tsconfigRootDir: import.meta.dirname }),
   prettier
 ];
